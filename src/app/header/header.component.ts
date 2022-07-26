85c1399064202e1eddebffb0e2a4b72614445983
Miracle Aghamobi <miracle.aghamobi@globalaccelerex.com>
2022-07-26 11:50:07 +0100

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title:string = 'Nova caculator';
+  title:string = 'Nova-calc';
   constructor() { }
 
   ngOnInit(): void {
